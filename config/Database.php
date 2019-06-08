433642cb58d5848ba4aeafe5d6bb0ac8903c1e01
Kbbansi <kwabenaampofo5@gmail.com>
2019-06-08 09:12:55 +0000

    Add ClearDB credentials

--- config/Database.php
+++ config/Database.php
@@ -8,21 +8,67 @@
 
 class Database {
 	//db params
-	private $host = 'localhost';
-	private $dbName = 'school-mapper';
-	private $user = 'root';
-	private $password = 'devionJave';
-
+	private $url;
 	private $conn;
 
+	/**
+	 * @return mixed
+	 */
+	public function getUrl() {
 
+		return getenv("CLEARDB_DATABASE_URL");
+	}
+
+	private $host;
+	private $user;
+	private $password;
+	private $db;
+
+	/**
+	 * @param mixed $host
+	 *
+	 * @return string host
+	 */
+	public function setHost( $host ) {
+		return $this->host = $this->url['host'];
+	}
+
+
+	/**
+	 * @param mixed $user
+	 *
+	 * @return string user
+	 */
+	public function setUser( $user ) {
+		return $this->user = $this->url['user'];
+	}
+
+
+	/**
+	 * @param mixed $password
+	 *
+	 * @return string password
+	 */
+	public function setPassword( $password ) {
+		return $this->password = $this->url['pass'];
+	}
+
+
+	/**
+	 * @param mixed $db
+	 *
+	 * @return bool|string
+	 */
+	public function setDb( $db ) {
+		return $this->db = substr($this->url['path'], 1);
+	}
 	//connect to db
 	public function connect() {
 		$this->conn = null;
 
 		//PDO Instance
 		try{
-			$this->conn = new PDO('mysql:host='.$this->host.';dbname='.$this->dbName,$this->user,$this->password);
+			$this->conn = new PDO('mysql:host='.$this->host.';dbname='.$this->db,$this->user,$this->password);
 			//set error
 			$this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 		} catch (PDOException $exception){
