f3331fb9739e126235d2892bea37c8c2d3a83450
kbbansi <kwabenaampofo5@gmail.com>
2020-10-27 23:03:37 +0000

    Change db credentials

--- config/Database.php
+++ config/Database.php
@@ -23,7 +23,7 @@ class Database {
 
 		//PDO Instance
 		try{
-			$this->conn = new PDO('mysql:host='.$this->host.';dbname='.$this->dbName.';port='.$this->port.';'.$this->user,$this->password);
+			$this->conn = new PDO('mysql://rdhl6ymnx8qsjlst:dikw7xehjno6gebv@klbcedmmqp7w17ik.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/a23uoirtxjsl5dea');
 			//set error
 			$this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 		} catch (PDOException $exception){
